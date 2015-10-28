b225d62c574de993ccace9faf26b2dd1e1dd8d6b
rhaonnor <rhaonnor@gmail.com>
2015-10-28 20:54:09 +0100

    Delete Matrix.bat

--- downloads/Matrix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Matrix
-color A
-mode 1000
-:top
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-goto top
